fcae93e3865047d38136efda39bf917d59c00a32
Stéfani Ritter <ritterstefani666@gmail.com>
2026-09-10 18:47:53 -0300

    Feature: cria função para comando ls

--- arquivo.py
+++ arquivo.py
@@ -1,7 +1,3 @@
-# Tamanho diretório raiz: 2 Blocos (8192 bytes)
-# 64 bytes pra cada arquivo
-# Número de arquivos suportado: 8192 / 64 = 128 arquivos
-
 import struct
 
 class Arquivo:
@@ -10,8 +6,8 @@ class Arquivo:
 
         self.nome = nome.encode("utf-8")[:32].ljust(32, b'\x00') # 32 bytes (32s no struct)
         self.tamanhoArquivo = tamanhoArquivo # 8 Bytes (q)
-        self.indiceFAT = indiceFAT # 8 Bytes (q)
-        self.status = status # 1 Byte (b) -> (Ex: 0=Livre, 1=Ocupado, 2=É um subdiretório)
+        self.indiceFAT = indiceFAT # 8 Bytes (q) # indice do bloco onde o arquivo começa na FAT
+        self.status = status # 1 Byte (b) -> (0=Livre, 1=Ocupado, 2=É um subdiretório)
         self.protecao = protecao # 1 Byte (b) -> para implementar o protect
         self.reservado = b'\x00' * 14 # Sobram 14 Bytes (14s) para usar para outras funcionalidades, como data de criação
 
